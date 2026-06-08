f747988119a66596051c8fa379563651fb1493a3
Dmytro Shutko <dima.shutko5@gmail.com>
2026-06-08 23:15:14 +0200

    fail tasks

--- task.ps1
+++ task.ps1
@@ -10,9 +10,9 @@ $dbSubnetIpRange = "10.20.30.64/26"
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
 
-$webnsgname = "nsg-" + $webSubnetName
-$mngnsgname = "nsg" + $mngSubnetName
-$dbnsgname = "nsg-" + $dbSubnetName
+$webnsgname = $webSubnetName
+$mngnsgname = $mngSubnetName
+$dbnsgname = $dbSubnetName
 
 $webnsgrulenameHTTP = "HTTP"
 $mngnsgrulenameSSH = "SSH"
@@ -29,14 +29,15 @@ Write-Host "Creating mngSubnet network security group..."
 Write-Host "Creating dbSubnet network security group..."
 # Write your code for creation of management NSG here -> 
 
-# NETWORK RULES
 $webnsgruleHTTP = New-AzNetworkSecurityRuleConfig -Name $webnsgrulenameHTTP -Access Allow -Protocol Tcp -Direction Inbound -Priority 110 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80, 443
+$webnsgruleInternal = New-AzNetworkSecurityRuleConfig -Name $webnsgrulenameHTTP -Access Allow -Protocol Tcp -Direction Inbound -Priority 110 -SourceAddressPrefix "10.20.30.0/24" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80, 443
 $mngnsgruleSSH = New-AzNetworkSecurityRuleConfig -Name $mngnsgrulenameSSH -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
+$mngnsgruleInternal = New-AzNetworkSecurityRuleConfig -Name $mngnsgrulenameSSH -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix "10.20.30.0/24" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
+$dbnsgruleInternal = New-AzNetworkSecurityRuleConfig -Name $mngnsgrulenameSSH -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix "10.20.30.0/24" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
 
-# NETWORK SECURITY GROUPS
-$webnsg = New-AzNetworkSecurityGroup -Name $webnsgname -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $webnsgruleHTTP
-$mngnsg = New-AzNetworkSecurityGroup -Name $mngnsgname -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $mngnsgruleSSH
-$dbnsg = New-AzNetworkSecurityGroup -Name $dbnsgname -ResourceGroupName $resourceGroupName -Location $location
+$webnsg = New-AzNetworkSecurityGroup -Name $webnsgname -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $webnsgruleHTTP, $webnsgruleInternal
+$mngnsg = New-AzNetworkSecurityGroup -Name $mngnsgname -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $mngnsgruleSSH, $mngnsgruleInternal
+$dbnsg = New-AzNetworkSecurityGroup -Name $dbnsgname -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $dbnsgruleInternal
 
 Write-Host "Creating a virtual network ..."
 $webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $webnsg
